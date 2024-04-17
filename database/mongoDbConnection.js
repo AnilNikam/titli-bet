43362da78651cdbb8dfdc4d4e288d185f33b0f08
ankit <iankitkhatrani@gmail.com>
2024-04-17 11:26:46 +0530

    database added

--- database/mongoDbConnection.js
+++ database/mongoDbConnection.js
@@ -7,6 +7,7 @@ const logger = require('../logger');
 const mongoDB = config.DATABASE;
 
 mongoose.connect(mongoDB, {
+  dbName: 'titlibetdb',
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
